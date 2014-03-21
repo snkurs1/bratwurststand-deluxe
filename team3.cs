38ee45d6fa3a7e88bc22d76046d83ff00e3bffb3
snkurs1 <SNKurs1@trash-mail.com>
2014-03-21 09:57:41 +0100

    Update team3.cs

--- team3.cs
+++ team3.cs
@@ -1,10 +1,24 @@
-  public class Schiffe
-  { 
-    int Groesse;
-    string Startposition;
-    AusrichtungsTyp Ausrichtung;
-  }
-  public enum AusrichtungsTyp
-  {
-      horizontal, vertikal
-  }
+public class Schiff
+    {
+        List<String> Koordinaten = new List<string>();
+        public Schiff(int Groesse, string Startposition,AusrichtungsTyp Ausrichtung)
+        {
+            char _v = Startposition[0]; 
+            char _h = Startposition[1];
+            for(int i = 0;i < Groesse;i++)
+            {
+                if(Ausrichtung == AusrichtungsTyp.vertikal)
+                {
+                    Koordinaten.Add( ((_v++).ToString() + _h) );
+                }
+                if(Ausrichtung == AusrichtungsTyp.horizontal)
+                {
+                    Koordinaten.Add( _v.ToString() + _h++);
+                }
+            }
+        }
+    }
+    public enum AusrichtungsTyp
+    {
+        horizontal, vertikal
+    }
