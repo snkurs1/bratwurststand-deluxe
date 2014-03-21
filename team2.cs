4a7eadfac4b17ed14b06068a6572e00effcf004f
snkurs1 <SNKurs1@trash-mail.com>
2014-03-21 10:27:57 +0100

    Update team2.cs

--- team2.cs
+++ team2.cs
@@ -1,1 +1,17 @@
-foobar
+class Treffer
+    {
+        public static List<Schuss> Fire(String Koordinate, List<Schiff> Schiffe, List<Schuss> OldSchots)
+        {
+            Schuss _shot = new Schuss();
+            _shot.Koordinaten = Koordinate;
+            _shot.getroffen = Schiffe.Exists(s => s.Koordinaten.Contains(_shot.Koordinaten)); ;
+
+            OldSchots.Add(_shot);
+            return OldSchots;
+        }
+    }
+public struct Schuss
+    {
+        public bool getroffen;
+        public String Koordinaten;
+    }
