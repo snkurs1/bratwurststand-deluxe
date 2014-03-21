1d0c17ec8169b73c4c8404fc0cec14a2c43c6eb7
snkurs1 <SNKurs1@trash-mail.com>
2014-03-21 10:48:00 +0100

    Update integration.cs

--- integration.cs
+++ integration.cs
@@ -22,14 +22,14 @@
 
 class Treffer
     {
-        public static List<Schuss> Fire(String Koordinate, List<Schiff> Schiffe, List<Schuss> OldSchots)
+        public static List<Schuss> Fire(String Koordinate, List<Schiff> Schiffe, List<Schuss> OldShots)
         {
             Schuss _shot = new Schuss();
             _shot.Koordinaten = Koordinate;
             _shot.getroffen = Schiffe.Exists(s => s.Koordinaten.Contains(_shot.Koordinaten));
 
-            OldSchots.Add(_shot);
-            return OldSchots;
+            OldShots.Add(_shot);
+            return OldShots;
         }
     }
 public struct Schuss
